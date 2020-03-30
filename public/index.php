90fcf08a9ded51c81e72da87c0d86bc14d0b0e11
Signetp <signepedersen97@live.dk>
2020-03-30 11:12:23 +0200

    Web based

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<head>
+    <title>Semester feed</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+
+<h1> Welcome to the beerproduction</h1>
+
+</body>
