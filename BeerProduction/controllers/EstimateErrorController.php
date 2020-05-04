15f4c165f0f5cd5463be79cc03fdb48cacf4ca95
TimmyMC <43139433+TimmyMC@users.noreply.github.com>
2020-05-04 12:25:52 +0200

    Moved data related functions to a model

--- BeerProduction/controllers/EstimateErrorController.php
+++ BeerProduction/controllers/EstimateErrorController.php
@@ -4,35 +4,18 @@ class EstimateErrorController extends Controller
 {
     public function index()
     {
-        // initial data
+        //initial page data
+        $productType = 0; //pilsner
+        $viewbag['machineSpeed'] = 400;
+        $viewbag['estimatedError'] = $this->model('EstimateError')->estimateErrorFunction($productType, $viewbag['machineSpeed']);
         $viewbag['maxSpeed'] = 600;
         $this->view('EstimateError/EstimateError', $viewbag);
     }
 
-    //TODO data is hardcoded...
     public function getMaxSpeed($productType)
     {
         if ($this->get()) {
-            switch ($productType) {
-                case 0:
-                    echo 600;
-                    break;
-                case 1:
-                    echo 300;
-                    break;
-                case 2:
-                    echo 150;
-                    break;
-                case 3:
-                    echo 200;
-                    break;
-                case 4:
-                    echo 100;
-                    break;
-                case 5:
-                    echo 125;
-                    break;
-            }
+            echo $this->model('EstimateError')->getMaxSpeed($productType);
         }
     }
 
@@ -40,33 +23,7 @@ class EstimateErrorController extends Controller
     public function estimateErrorFunction($productType, $machineSpeed)
     {
         if ($this->get()) {
-            $estimatedError = 0;
-            switch ($productType) {
-                case 0:
-                    $estimatedError = 0.113 * exp(0.0103 * $machineSpeed) . " ";
-                    break;
-                case 1:
-                    $estimatedError = 0.336 * $machineSpeed - 1.01;
-                    break;
-                case 2:
-                    $estimatedError = 35.1 - 1.35 * $machineSpeed + 0.0122 * $machineSpeed ** 2;
-                    break;
-                case 3:
-                    $estimatedError = 43 + 0.124 * $machineSpeed - 0.0014 * $machineSpeed ** 2;
-                    break;
-                case 4:
-                    $estimatedError = 1.29 - 0.0706 * $machineSpeed + 0.00419 * $machineSpeed ** 2;
-                    break;
-                case 5:
-                    $estimatedError = 0.402 * $machineSpeed + 9.07;
-                    break;
-            }
-            if ($estimatedError < 0) {
-                $estimatedError =  0;
-            } else if ($estimatedError > 100) {
-                $estimatedError = 100;
-            }
-            echo $estimatedError . "%";
+            echo $this->model('EstimateError')->estimateErrorFunction($productType, $machineSpeed);
         }
     }
 }
--- BeerProduction/views/EstimateError/EstimateError.php
+++ BeerProduction/views/EstimateError/EstimateError.php
@@ -4,7 +4,7 @@
 <p>Production Speed:</p>
 <form>
     <label for="beerType">Choose a beer:</label>
-    <select id="beerType" name="beerType" onChange="setMaxSpeed(this.value)">
+    <select id="beerType" name="beerType" onchange="setMaxSpeed(this.value)">
         <option value=0>Pilsner</option>
         <option value=1>Wheat</option>
         <option value=2>IPA</option>
@@ -14,11 +14,12 @@
     </select>
     </br>
     <label for="productionSpeedSlider">0</label>
-    <input id="productionSpeedSlider" type="range" min="1" max="<?= $viewbag['maxSpeed'] ?>" oninput="getEstimatedError(beerType.value, this.value)">
+    <input id="productionSpeedSlider" type="range" min="1" max="<?= $viewbag['maxSpeed'] ?>" value="<?= $viewbag['machineSpeed'] ?>" oninput="getEstimatedError(beerType.value, this.value)">
+    <!-- TODO oninput creates a massive amount of ajax calls which may lag due to the cisco vpn, if this happens change it to onchange -->
     <label id="maxSpeedLabel" for="productionSpeedSlider"><?= $viewbag['maxSpeed'] ?></label>
 </form>
 
-The estimated error is: <p id="errorEstimate"><?= $viewbag['pilsner'] ?? "" ?></p></br>
+The estimated error is: <p id="errorEstimate"><?= $viewbag['estimatedError'] ?></p></br>
 
 <script src="/js/estimateErrorAjax.js"></script>
 <?php include '../BeerProduction/views/partials/footer.php'; ?>
